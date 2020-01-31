53dbef561e97442766c171cdf4c2cfa9ffc166f7
Adrian Samaniego <llathander@gmail.com>
2020-01-31 01:02:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |x|
+   "Hello, my name is #{x}."
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |x, y|
+    "Hello, #{x}! You'll be assigned to room #{y+1}!"
+  end
+end
+
+
+    
